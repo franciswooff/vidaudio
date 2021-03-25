d14ba2c785e97c698f42a89ff77dd58e09f81f50
FrancisW <65406277+franciswooff@users.noreply.github.com>
2021-03-25 15:45:10 +0000

    Update preview.php
    Further attempt to beautify html output

--- preview.php
+++ preview.php
@@ -31,13 +31,13 @@ $ano = $anox-2;
 
 echo "<p>Your EDITME text file specifies you will use <b>".$vidnum."</b> videos for your test &amp; each video will have <b>".$trxnum."</b> audio tracks for subjects to rate (you will need <b>".$aflsno."</b> audio files). See the GitHub README for how to name these to appear below.</p>
 <p>Your 'videofiles' folder currently contains <b>".$vno."</b> files &amp; your 'audiofiles' folder <b>".$ano."</b> files (your test is set up using the figures from your EDITME.txt file, the numbers of files you have uploaded currently is shown for checking).</p>
-<p>n.b. your videos shouldn't show here much more than half the width of the page (on a landscape desktop PC monitor). If wider that's probably too large for (our) use over the net. Something in the range 360 (e.g. 640x360) to 540 (e.g. 960x540) should be enough. No bigger than necessary, on videos especially, is crucially good net etiquette.</p>";
+<p>n.b. your videos shouldn't show here much more than half the width of the page (on a landscape desktop PC monitor). If wider that's probably too large for (our) use over the net. Something in the range 360 (e.g. 640x360) to 540 (e.g. 960x540) should be enough. No bigger than necessary, on videos especially, is crucially good net etiquette.</p>
+";
 
 for ($v = 1; $v <= $vidnum; $v++){
   echo '<video controls><source src="videofiles/'.$v.'.mp4" type="video/mp4"></video>
-  <label>Video '.$v.'</label>
-  
-  <p>The following audio tracks will be available with this video</p>';
+<label>Video '.$v.'</label>
+<p>The following audio tracks will be available with this video</p>';
   for ($a = 1; $a <= $trxnum; $a++){
     echo '<audio controls><source src="audiofiles/'.$v.'_'.$a.'.wav" type="audio/mpeg"></audio>
     <label>Audio '.$v.'_'.$a.'</label>
@@ -46,18 +46,18 @@ for ($v = 1; $v <= $vidnum; $v++){
     
   $serus = array_search($v,$csv);
   
-  if (is_int($serus)){
-    echo '<p>You have selected to turn off randomisation of mapping of audio tracks to rating sliders for this test page via line 4 of EDITME.txt</p>
-    ';
-    } else {
-    echo '<p>These audio tracks will be randomly mapped to the rating sliders on the test page (if you wish to turn this randomisation off for this page see step 11. GitHub README or examples in EDITME.txt)</p>
-    ';
-    }  
-  
-  echo '<p>The following additional paragraph(s) will appear below the standard instructions on this test page (though not in bold, that is to make it clear what is extra text here):<br><b>'.file_get_contents("extras/".$v.".txt").'</b></p>
-  <p>The following scale label will appear against the faders on this test page (a border is shown here so you can see the labels are correctly mapped against the 5 available lines):</p>
+if (is_int($serus)){
+  echo '<p>You have selected to turn off randomisation of mapping of audio tracks to rating sliders for this test page via line 4 of EDITME.txt</p>
   ';
-  $tblc = fopen('labels/'.$v.'.txt', 'r');
+  } else {
+  echo '<p>These audio tracks will be randomly mapped to the rating sliders on the test page (if you wish to turn this randomisation off for this page see step 11. GitHub README or examples in EDITME.txt)</p>
+  ';
+  }
+  
+echo '<p>The following additional paragraph(s) will appear below the standard instructions on this test page (though not in bold, that is to make it clear what is extra text here):<br><b>'.file_get_contents("extras/".$v.".txt").'</b></p>
+<p>The following scale label will appear against the faders on this test page (a border is shown here so you can see the labels are correctly mapped against the 5 available lines):</p>
+';
+$tblc = fopen('labels/'.$v.'.txt', 'r');
 $t1 = fgets($tblc);
 $t2 = fgets($tblc);
 $t3 = fgets($tblc);
@@ -72,9 +72,9 @@ echo '<table>
     <tr><td>'.$t4.'</td></tr>
     <tr><td>'.$t5.'</td></tr>
   </table>
-  ';
-  
+';
 }
+
 echo '<p>At the end of the test send the results to this e-mail address: <b>'.$mail.'</b></p>';
 ?>
 
