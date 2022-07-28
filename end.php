a6d33728fe96292bb7e1532ad2e8c80affb38292
FrancisW <65406277+franciswooff@users.noreply.github.com>
2022-07-28 17:16:29 +0100

    Update end.php
    Project reworked with new feature set

--- end.php
+++ end.php
@@ -6,7 +6,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="main.css">
 <link rel="stylesheet" type="text/css" href="page.css">
-<script src="vidaud.js" defer></script>
 </head>
 
 <body>
@@ -16,7 +15,7 @@
 <p>Thanks for taking part in the test. Your results have been submitted.</p>
 
 <?php
-$editme = fopen("EDITME.txt", "r") or die('<p>Unable to open your EDITME.txt file</p>');
+$editme = fopen("EDITME.txt", "r") or die('<h1>Unable to open your EDITME.txt file</h1>');
 $vidnum=fgets($editme);
 $trxnum=fgets($editme);
 $mail=fgets($editme);
@@ -24,16 +23,14 @@ fclose($editme);
 
 session_start();
 $subno = $_SESSION["subno"];
-//echo "participant no ".$subno."<br>";
 
 for ($evc = 1; $evc <= $vidnum; $evc++){
   for ($f = 1; $f <= $trxnum; $f++){
-    $addr = $evc.'_'.$f;  
+    $addr = $evc.'_'.$f;
     $res = $_SESSION[$addr];
     $allres .= "\r\n".$res;
-    }
+  }
 }
-//echo $allres;
 
 mail($mail, "Vidaudio test results for participant number ".$subno, $allres);
 
